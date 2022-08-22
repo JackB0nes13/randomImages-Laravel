bba5360ca74993d75ab824b6212c6e3d7700a23e
JackB0nes13 <jackbenjaminjones13@gmail.com>
2022-08-22 17:19:53 +0100

    done

--- RandomImages/app/Http/Controllers/Images.php
+++ RandomImages/app/Http/Controllers/Images.php
@@ -5,26 +5,27 @@ use App\Http\Controllers\Images;
 
 use Illuminate\Http\Request;
 
+
 class Images extends Controller
 {
+
+    public $values = ['http://127.0.0.1:8000/images/noble.jpg', 'http://127.0.0.1:8000/images/city.jpg' , 'http://127.0.0.1:8000/images/soldier.jpg', 'http://127.0.0.1:8000/images/walls.jpg'];
 
 
 
     public function descending() {
-        $values = ['http://127.0.0.1:8000/images/noble.jpg', 'http://127.0.0.1:8000/images/city.jpg' , 'http://127.0.0.1:8000/images/soldier.jpg', 'http://127.0.0.1:8000/images/walls.jpg'];
-        sort($values);
-        print_r(json_encode($values));
+        sort($this->values);
+        print_r(json_encode($this->values));
     }
 
     public function ascending() {
-        $values = ['http://127.0.0.1:8000/images/noble.jpg', 'http://127.0.0.1:8000/images/city.jpg' , 'http://127.0.0.1:8000/images/soldier.jpg', 'http://127.0.0.1:8000/images/walls.jpg'];
-        rsort($values);
-        print_r(json_encode($values));
+
+        rsort($this->values);
+        print_r(json_encode($this->values));
     }
 
     public function random() {
-        $values = ['http://127.0.0.1:8000/images/noble.jpg', 'http://127.0.0.1:8000/images/city.jpg' , 'http://127.0.0.1:8000/images/soldier.jpg', 'http://127.0.0.1:8000/images/walls.jpg'];
-        shuffle($values);
-        print_r(json_encode($values));
+        shuffle($this->values);
+        print_r(json_encode($this->values));
     }
 }
